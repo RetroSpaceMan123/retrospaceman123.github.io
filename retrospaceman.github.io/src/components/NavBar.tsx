11686319531e87e2dc964f83aa8306ad4e39bab1
RetroSpaceMan123 <retrospaceman123@gmail.com>
2026-01-26 18:02:09 -0500

    Implement NavBar

--- retrospaceman.github.io/src/components/NavBar.tsx
+++ retrospaceman.github.io/src/components/NavBar.tsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+import '../css/NavBar.css'
+
 function NavBar() {
     return (
-        <>
-        </>
+    <nav>
+        <ul>
+            <Link to='/' className='nav-link'>Home</Link>
+            <Link to='/about' className='nav-link'>About</Link>
+            <Link to='/projects' className='nav-link'>Projects</Link>
+            <Link to='/contact' className='nav-link'>Contact Me</Link>
+        </ul>
+    </nav>
     )
 }
 
