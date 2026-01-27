6a44b3ccd90c042ca37553174a10f9e69af5b965
Carlos Avila <48300738+RetroSpaceMan123@users.noreply.github.com>
2026-01-27 16:08:54 -0500

    Implement Header

--- retrospaceman.github.io/src/components/Header.tsx
+++ retrospaceman.github.io/src/components/Header.tsx
@@ -1,6 +1,14 @@
+import NavBar from "./NavBar";
+import '../css/Header.css';
+
 function Header() {
     return (
         <>
+            <header>
+                <img src="assets/pfpspace.png" alt="RetroSpaceMan123" />
+                <h1>RetroSpaceMan123.io</h1>
+            </header>
+            <NavBar />
         </>
     )
 }
