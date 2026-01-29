aa37c8267640d25c0208e18a69aac0ce062011fc
Carlos Avila <48300738+RetroSpaceMan123@users.noreply.github.com>
2026-01-29 15:09:20 -0500

    Implement About Page

--- retrospaceman.github.io/src/pages/About.tsx
+++ retrospaceman.github.io/src/pages/About.tsx
@@ -1,6 +1,15 @@
 function About() {
     return (
         <>
+            <div className="about">
+                <h2>About Me</h2>
+                <img src="./src/assets/pfpspace.png" alt="RetroSpaceMan123" />
+                <p className="subtitles">An old profile picture I made in 2021</p>
+                <p>Hello! My name is Carlos. I am a recent Computer Science graduate from the University of Florida. I have earned an Associate's of Arts from Santa Fe College, and I just earned a Bachelor's of Science from the College of Engineering at the University of Florida.</p>
+                <p>Although I have been programming since my childhood, I started consistently programming in 2018 with the creation of my first Discord bot. Since then I have had the privilege to work on a series of projects both on my own and with a team.</p>
+                <p>I have also completed an internship at Miami-Dade County ITD during the summer of 2023, which taught me how software engineers operate in a real-world setting.</p>
+                <p>I am currently looking for a full-time position for a software engineer to start working in a field I feel passionate about.</p>
+                </div>
         </>
     )
 }
