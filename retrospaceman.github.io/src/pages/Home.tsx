6306898f5c76434ea712e3cfd931f47a446ceadc
RetroSpaceMan123 <retrospaceman123@gmail.com>
2026-01-30 12:44:20 -0500

    Add Disclaimer About Site Transformation.

--- retrospaceman.github.io/src/pages/Home.tsx
+++ retrospaceman.github.io/src/pages/Home.tsx
@@ -3,6 +3,7 @@ function Home() {
         <div className="home">
             <h2>Welcome to RetroSpaceMan123.io!</h2>
             <p>This is the personal website for Carlos Leonardo Avila, also know as RetroSpaceMan123. Here you can see the programming projects I have been a part of throughout the years.</p>
+            <p>This site is currently undergoing a redesign using modern web dev tools. Changes in this website will be more common until the transformation is complete.</p>
             <p>Click on one of the tabs in the navigation bar above to find more information.</p>
         </div>
     )
