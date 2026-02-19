66a048f3c97d0840b65970222de65bb699eb9b15
Carlos Avila <48300738+RetroSpaceMan123@users.noreply.github.com>
2026-02-19 14:26:49 -0500

    Simplify Home Text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div className="page-content">
             <h2>Welcome to RetroSpaceMan123.io!</h2>
-            <p>This is the personal website for Carlos Leonardo Avila, also know as RetroSpaceMan123. Here you can see the programming projects I have been a part of throughout the years.</p>
+            <p>This is my personal website where you can see the programming projects I have been a part of throughout the years.</p>
             <p>If you wish to learn more about me, click on the About tab in the navigation bar above.</p>
             <p>If you wish to see the projects I have worked on, click on the Projects tab in the navigation bar above.</p>
             <p>If you wish to contact me, click on the Contact tab in the navigation bar above.</p>
